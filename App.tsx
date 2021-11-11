587c9071fac3c94e470a50eff6006acf8ba97077
Mateus Leonardo <m4teusmatos@gmail.com>
2021-11-10 22:19:32 -0300

    :hammer: refactor the index main

--- App.tsx
+++ App.tsx
@@ -1,21 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { View, Text, StyleSheet, StatusBar } from "react-native";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+const App = () =>{
+    return(
+        <View style={styles.container}>
+            <StatusBar />
+            <Text>New Project</Text>
+        </View>
+    )
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    container: {
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        flex: 1
+    }
+})
+
+export default App;
