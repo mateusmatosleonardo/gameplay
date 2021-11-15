cd453ce6ed4dd932c79ca1a90539e7cb7cfd0c16
Mateus Leonardo <m4teusmatos@gmail.com>
2021-11-15 13:21:24 -0300

    :rocket: importing fots

--- App.tsx
+++ App.tsx
@@ -1,5 +1,8 @@
 import React from "react";
-import { View, StatusBar } from "react-native";
+import { useFonts } from 'expo-font';
+import {Inter_400Regular, Inter_500Medium} from '@expo-google-fonts/inter';
+import {Rajdhani_500Medium, Rajdhani_700Bold} from '@expo-google-fonts/rajdhani';
+
 import Signin from "./src/screens/Signin";
 
 const App = () =>{
@@ -7,7 +10,5 @@ const App = () =>{
         <Signin/>
     )
 }
-
-
 
 export default App;
