cd018c64ad361467965cc0f674f56eec17f13a68
Mateus Leonardo <m4teusmatos@gmail.com>
2021-11-15 13:36:42 -0300

    :construction: add new fonts and colors

--- App.tsx
+++ App.tsx
@@ -2,21 +2,26 @@ import React from "react";
 import { useFonts } from 'expo-font';
 import {Inter_400Regular, Inter_500Medium} from '@expo-google-fonts/inter';
 import {Rajdhani_500Medium, Rajdhani_700Bold} from '@expo-google-fonts/rajdhani';
+import AppLoading from 'expo-app-loading';
 
 import Signin from "./src/screens/Signin";
 
 const App = () =>{
 
-    useFonts({
+    const [fontsLoaded] = useFonts({
         Inter_400Regular,
         Inter_500Medium,
         Rajdhani_500Medium,
         Rajdhani_700Bold
     });
 
+    if(!fontsLoaded){
+        return (<AppLoading/>);
+    };
+
     return(
         <Signin/>
-    )
-}
+    );
+};
 
 export default App;
--- src/global/styles/theme.ts
+++ src/global/styles/theme.ts
@@ -4,5 +4,27 @@ export const theme = {
         heading: "#DDE3FA",
         primary: "#E51C44",
         line: "#991F36",
-    }
+
+        secondary100: '#0A1033',
+        secondary90: '#0D133D',    
+        secondary80: '#0E1647',
+        secondary85: '#171F52',
+        secondary70: '#1B2565', 
+        secondary60: '#1B2565',
+        secondary50: '#243189', 
+        secondary40: '#1D2766', 
+        secondary30: '#495BCC', 
+   
+        overlay: 'rgba(0,0,0,0.7)',
+        highlight: '#ABB1CC',
+        on: '#32BD50',
+
+    discord: '#7289da',
+    },
+    fonts: {
+        title700: 'Rajdhani_700Bold',
+        title500: 'Rajdhani_500Medium',
+        text400: 'Inter_400Regular',
+        text500: 'Inter_500Medium',
+      }
 }
