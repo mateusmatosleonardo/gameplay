bb31bf1074f158c29e530c6fa3bc93188a08d67c
Mateus Leonardo <m4teusmatos@gmail.com>
2021-11-16 19:26:35 -0300

    :art: align container the profile

--- src/components/Avatar/styles.ts
+++ src/components/Avatar/styles.ts
@@ -4,10 +4,10 @@ import { theme } from "../../global/styles/theme";
 
 export const styles = StyleSheet.create({
     container: {
-        width: 50,
-        height: 50,
+        width: 52,
+        height: 52,
         borderRadius: 8,
-        backgroundColor: theme.colors.secondary70,
+        backgroundColor: theme.colors.secondary50, //mudar a cor depois
         alignItems: 'center',
         justifyContent: 'center',
         marginRight: 22
@@ -15,6 +15,6 @@ export const styles = StyleSheet.create({
     avatar: {
         width: 46,
         height: 46,
-        borderRadius: 8
+        borderRadius: 6
     }
 })  
